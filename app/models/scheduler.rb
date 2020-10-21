b973a3386b394e6530af9de6593f729f75978db8
= <mail.sparsha@gmail.com>
2020-10-21 11:38:48 -0400

    Implement java code execution directly after scheduling

--- app/models/scheduler.rb
+++ app/models/scheduler.rb
@@ -1,5 +1,19 @@
 class Scheduler < ActiveRecord::Base
-  has_one :test_suite
+  belongs_to :test_suite
+
+  after_create :schedule_created
+
+  def schedule_created
+    status = self.test_suite.status
+    if !status.nil? && status.downcase=='final'
+       tester_path = self.test_suite.environment.selenium_tester_url
+       if !tester_path.nil?
+          scheduler_id = self.id
+          mode = "headless"
+          system("#{tester_path} #{mode} #{scheduler_id}")
+       end
+    end
+  end
 
   def self.create_new_schedule(suite_id, date)
     schedule = Scheduler.new
