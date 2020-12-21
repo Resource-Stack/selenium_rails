acbd3251f8fc9f762e82848d5a49490a3116208a
= <mail.sparsha@gmail.com>
2020-12-21 15:22:00 -0500

    Implement javascript editor in test case edit mode

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,15 @@
 Rails.application.config.assets.precompile += %w( drawflow.js )
 Rails.application.config.assets.precompile += %w( font-awesome.js )
 Rails.application.config.assets.precompile += %w( drawflow.css )
+
+Rails.application.config.assets.precompile += %w( editor/ace.js )
+Rails.application.config.assets.precompile += %w( editor/ext-tern.js ) 
+Rails.application.config.assets.precompile += %w( editor/snippets/javascript.js ) 
+Rails.application.config.assets.precompile += %w( editor/mode-javascript.js ) 
+Rails.application.config.assets.precompile += %w( editor/theme-chrome.js ) 
+Rails.application.config.assets.precompile += %w( editor/worker-tern.js ) 
+Rails.application.config.assets.precompile += %w( editor/worker-javascript.js ) 
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
